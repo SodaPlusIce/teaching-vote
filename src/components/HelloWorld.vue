a8cef729cd1099d58c76da31d9cad58f95071c2e
昝崇崇 <213201535@seu.edu.cn>
2022-11-02 20:06:09 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,184 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span style="font-size: 22px"
+          >哈喽呀，各位小可爱，快选出你认为是五大基本地形的选项吧！</span
+        >
+      </div>
+      <div>
+        <el-checkbox
+          class="aaa"
+          v-model="checked0"
+          label="冰川"
+          border
+        ></el-checkbox>
+        <el-checkbox
+          class="aaa"
+          v-model="checked1"
+          label="平原"
+          border
+        ></el-checkbox
+        ><br />
+        <el-checkbox
+          class="aaa"
+          v-model="checked2"
+          label="沙漠"
+          border
+        ></el-checkbox>
+        <el-checkbox
+          class="aaa"
+          v-model="checked3"
+          label="盆地"
+          border
+        ></el-checkbox
+        ><br />
+        <el-checkbox
+          class="aaa"
+          v-model="checked4"
+          label="湿地"
+          border
+        ></el-checkbox>
+        <el-checkbox
+          class="aaa"
+          v-model="checked5"
+          label="湖泊"
+          border
+        ></el-checkbox
+        ><br />
+        <el-checkbox
+          class="aaa"
+          v-model="checked6"
+          label="丘陵"
+          border
+        ></el-checkbox>
+        <el-checkbox
+          class="aaa"
+          v-model="checked7"
+          label="高原"
+          border
+        ></el-checkbox
+        ><br />
+        <el-checkbox
+          class="aaa"
+          v-model="checked8"
+          label="峡谷"
+          border
+        ></el-checkbox>
+        <el-checkbox
+          class="aaa"
+          v-model="checked9"
+          label="山地"
+          border
+        ></el-checkbox
+        ><br />
+        <el-checkbox
+          class="aaa"
+          v-show="false"
+          label="峡谷"
+          border
+        ></el-checkbox>
+        <el-checkbox
+          class="aaa"
+          v-show="false"
+          label="峡谷"
+          border
+        ></el-checkbox>
+      </div>
+      <div>
+        <el-button type="primary" style="margin-top: 10px" @click="commit()"
+          >提交结果</el-button
+        >
+      </div>
+    </el-card>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      checked0: false,
+      checked1: false,
+      checked2: false,
+      checked3: false,
+      checked4: false,
+      checked5: false,
+      checked6: false,
+      checked7: false,
+      checked8: false,
+      checked9: false,
+    };
+  },
+  methods: {
+    commit() {
+      //1 3 6 7 9
+      if (
+        this.checked0 == false &&
+        this.checked1 == true &&
+        this.checked2 == false &&
+        this.checked3 == true &&
+        this.checked4 == false &&
+        this.checked5 == false &&
+        this.checked6 == true &&
+        this.checked7 == true &&
+        this.checked8 == false &&
+        this.checked9 == true
+      ) {
+        this.$message({
+          showClose: true,
+          duration: 0,
+          message: "太棒啦，恭喜你，答案全部正确！",
+          type: "success",
+        });
+      } else {
+        this.$message({
+          showClose: true,
+          duration: 0,
+          message: "有点小问题哟！快看看正确答案吧！",
+          type: "warning",
+        });
+      }
+      //显示正确答案
+      this.checked0 = false;
+      this.checked1 = true;
+      this.checked2 = false;
+      this.checked3 = true;
+      this.checked4 = false;
+      this.checked5 = false;
+      this.checked6 = true;
+      this.checked7 = true;
+      this.checked8 = false;
+      this.checked9 = true;
+    },
+  },
+};
 </script>
+<style>
+.aaa {
+  margin: 5px;
+}
+.text {
+  font-size: 14px;
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+.item {
+  margin-bottom: 18px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.clearfix:after {
+  clear: both;
 }
-a {
-  color: #42b983;
+
+.box-card {
+  width: 680px;
+  /* text-align: center; */
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 60px;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: "./",
+  // devServer: {
+  //   proxy: {
+  //     '/api': {                                // 要代理的接口名
+  //       target: 'http://127.0.0.1:5000',   // 要代理的接口地址
+  //       //           10.203.252.108           192.168.80.1
+  //       changeOrigin: true,                            // 允许跨域
+  //       pathRewrite: { '^/api': '' }            // 接口名重写
+  //     }
+  //   }
+  // }
+};
